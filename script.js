5a3fcafbeba6319dd14e54748faeac372891ec55
Dustsson <122706048+Dustsson@users.noreply.github.com>
2023-01-15 19:31:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+$("#elso").click(function () {
+    let val = 1;
+    val = Math.floor(Math.random() * 8 )
+    
+
+switch (val) {
+    case 0:
+        console.log(0)
+        $('#message').html('<b>Azt gondolom, hogy ezt a munkát - elsősorban - csakis jókedvvel lehet csinálni, másképp sehogy. Tehát valamivel próbáljátok minél többször (főleg nap elején) feldobni magatokat egy pozitív gondolattal.</b><br>Pl. azzal, hogy <i>mennyire csodálatos dolog az, ráadásul nagyon menő segíteni állami gondozott gyerekeknek.</i> Mert ez egy nagyon jó dolog, hogy ehhez mi is hozzá tudunk járulni.<br><b>Amit szintén nagyon fontosnak tartok, hogy mosolyogjatok, amennyit csak tudtok! Higgyétek el, hogy a mosoly semmibe sem kerül, viszont rengeteget hoz!</b><br>És soha ne azt számoljátok, hogy hányan mondtak nemet, hanem, hogy hányan igent.<br><br>Én minden hónap elején úgy indulok el dolgozni, hogy eszem ágában sincs az, hogy más szintet kéne elérjek, mint a maximum. Egyszerűen nem is létezik más az én fejemben. Ha ti is úgy álltok neki, hogy egy kitűzött szintet eléritek, akkor sosem fog ez másképp történni, <b>csak azt figyeljétek mindig, hogy mennyi hiányzik még hozzá, és ez napról napra kevesebb lesz.</b>  Higgyétek el, működik!<br><br><i> - Reni (Pest)</i>')
+        $("#kep").attr("src","broken.png")
+        $("#elso")
+        break
+    case 1:
+        console.log(1)
+        $('#message').html("<b>A tartós jó teljesítmény titka az, hogy mindig tűzz ki kisebb célokat magad elé 🙂</b><br>Ha például szeretnéd megírni az 1. szintet és heti 2 napot jössz, az csak napi 3500 Ft támogatás. <br>Ugye, már nem is olyan nehéz?<br><b>Tegyél meg mindent azért, hogy a kisebb céljaid elérd, és mire feleszmélsz már ott is állsz a dobogón 😎</b><br><br><i>- Gábor (Pest)</i>")
+        $("#kep").attr("src","broken.png")
+        break
+    default:
+        console.log(1000)
+        $('#message').html("")
+        $("#kep").attr("src","broken.png")
+        break
+    case 2:
+        console.log(2)
+        $('#message').html("Szurkolok, hogy mindenkinek napos (és talán nem fagyos) januárban legye része, ugyanis most is készülni kell ám soksok egésszel, és remélem a szerencse veletek lesz, tegyetek meg mindent, a városotokért, és magatokért is!<br><b>Csak pozitívan és energikusan, a január is egy hónap sok új lehetőséggel, hátha az emberek is így állnak hozzá  segítségnyújtásból, szóval csodaszép napot mindenkinek! </b><br><br><i>– Zoé (Szeged)</i>")
+        $("#kep").attr("src","broken.png")
+        break
+    case 3:
+        console.log(3)
+        $('#message').html("<b>Ami szerintem a legfontosabb, hogy ez egy olyan munka, amit nem lehet rossz kedvvel, motiválatlanul csinálni. Higgyétek el, nagyon fontos az őszinte mosoly, hiszen az emberek egyszerűen megérzik, ha rossz a légkör körülöttetek.<br></b>Ne várjatok el túl sokat magatoktól. Kisebb célokat tűzzetek ki minden napra, azt sokkal könnyebb lesz majd elérni. <br><i>Step by step, day by day :))</i><br><b>Ne ítéljetek külsőről!</b>Nem lehet senkinek sem megállapítani az anyagi helyzetét, sem pedig a viszonyát a gyermekekhez csak külső alapján. Én múlt héten több olyan egészt írtam, akikben semmi reményem nem volt, és mégis sikerült, sőt nem is kis összeggel. Úgyhogy, hajrá, mindenkit szólítgassatok!😌<br><br>Sokatoknál vettem észre, hogy ha elutasítást kap, lelombozódik, vagy végig sem mondja a leszólítást.<br><b>A “Szia, van két perced gyermekvédelemre?” mondatot mindig mondjátok végig, akkor is, ha már a sziánál rávágja, hogy nem!</b><br>Nagyon sok olyannal találkoztam mióta itt dolgozom, hogy elutasítottak, majd mikor meghallották, hogy gyermekvédelem visszafordultak.<br><br><i> - Vani (Pest)</i>")
+        $("#kep").attr("src","broken.png")
+        break
+    case 4:
+        console.log(4)
+        $('#message').html('Én is úgy gondolom, hogy optimizmussal lehet kihozni ebből a melóbol a maximumot, úgyhogy állítsátok át a mentalitásotokat, ha eddig másképp álltatok volna ehhez a melóhoz🙄 Több kutatás is bizonyítja, hogy tényleg csak a HOZZÁÁLLÁSOTOKON múlik a kedvetek!!<br>      <b>Higgyetek el, és bízzatok magatokban, hogy bármire képesek vagytok, amit eldöntötök!💪🏻</b> Illetve, <b>ha kedvesen es türelemmel álltok az emberekhez, ők is úgy fognak hozzátok!</b> Nem tudhatjátok, hogy milyen napjuk van, es lehet, hogy pont a ti kedves “szia”-tok hatására fognak megállni. Ha pedig goromba válaszokat kaptok reakcióként, csak engedjétek el, nevessetek rajta egy jót, osszátok meg velünk🤗 <br><b>Higgyétek el, ha ilyen könnyedséggel és pozitivitással álltok a mindennapjaitokhoz, ezt érezni fogják rajtatok az emberek is, és szívesebben is fognak megállni!</b><br><br>Fontos az is, hogy tartsatok pihiket is, igyatok egy jóóó kávét, egyetek egy fincsi csokis fánkot, hogy aztán újult erővel tudjátok folytatni ezt a csodás melót🥰<br>Hallgassátok meg a kedvenc pörgős, vidám dalaitokat, mert a zene és annak a dalszövege is nagy hatással van a hangulatunkra🎧<br>Táncoljatok is rá egyet, ha van rá lehetőségetek💃🏻<br>Vagy csak csukjátok be a szemeteket, és képzeljétek el, ahogy éppen a 2., 3., 4., stb. egészeteket írjátok ma✍🏻<br>Meló közben pedig inkább a jó dolgokra és kedves emberekre fókuszáljatok, más meg se érdemli a figyelmeteket💁🏻‍♀️<br>            Aranyozzátok be az emberek napját, hogy a tiéteket is bearanyozzák✨ Hajrá mindenkinek, szuperügyesek lesztek!💙<br><br><i>- Regi (Pest)</i>')
+        $("#kep").attr("src","broken.png")
+        break
+    case 5:
+        console.log(5)
+        $('#message').html('<b>Ne feszüljetek ma rá a munkára (meg amúgy soha nem kell, de ma tilos); az emberek látni fogják rajtatok, ha feszengtek.</b><br>Simán lehet, hogy ma nem lesz semmi (bár írni TÉNYLEG bármilyen helyszínen lehet), de legalább élvezitek, amit csináltok, ráadásul értelme is van<br><br>Már csak az a tény, hogy a csapatban vagytok, azt jelenti, hogy hozzájárultatok ahhoz a 40.000 egészhez, ami nemrég meglett. Csak képzeljétek el, hogy mennyit segített már a munkátok az alapítványnak.<br>:)<br><br><i>- Peti (Pest)</i>')
+        $("#kep").attr("src","broken.png")
+        break
+    case 6:
+        console.log(6)
+        $('#message').html("Én igazából azt imádom ebben a munkában hogy mindennap mást hoz.<br>Azt vettem észre, hogy sokan rászoronganak már nap elején, hogy egészt kéne írni, de még mindig van rengeteg ember, aki nem hallott erről a tevékenységről és baromira megtetszik neki ez a dolog.<br>Éppen ezért én nem úgy állok hozzá egy naphoz, hogy lesz-e egyáltalán valaki, hanem, hogy akár meglehet 2-3 egész is, és még egy 15 ezres is bármikor jöhet. Még mindig rengeteg olyan ember is van, aki akár hallott rólunk, de erről a támogatási lehetőségről soha, ezért nagyon jó, hogy ti vagytok, mert lehet mindig is akarta támogatni, de nem ismerte.<br>Rengetegszer lett úgy nagy összegű egészem, hogy tényleg semmit nem változtattam a spielemen. Egyszerűen csak az az adott ember még nem ismerte és baromira megtetszett neki, vagy tényleg épp imádja az SOS-t és nem is tudta, hogy van lehetőség havonta segíteni. Ugyanígy vagyok egy hónap elején is, hogy nem az, hogy vajon meglesz-e az átlag, hanem hogy vajon most melyik szintig jutok el😁<br><br>Már volt szó arról, hogy maga a leszólításban legyetek lelkesek, de szerintem baromi fontos, hogy maga az alapítvány tevékenységéért is lelkesedjetek, és az adott járókelőnek is azt kell átadni, hogy ti ezért nagyon oda vagytok. Hiszen valaki egy olyan dolgot szeretne majd támogatni, amit ti is támogatnátok. Sokszor nekem is nehéz, de kicsit elkell felejteni, hogy a pénzért dolgozol, és inkább arra gondolni, hogy másokon segítesz. Nagyon sokszor úgy írtam egészt, hogy én is azt vettem észre magamon, hogy nem egy robotos betanult szöveget mondtam egy embernek, hanem mintha egy barátnőmnek mesélném lelkesen, hogy milyen jó, hogy családban vannak a gyerekek stb. Hogyha kicsit már beleszürkültetek a munkába, szerintem érdemes felmenni most az SOS Instagram oldalára, mert tök jó posztokat tesznek fel. És ez kicsit újra fel tud lelkesíteni, hogy milyen jó ügyért dolgoztok.<br><br>A meggyőzésről akartam még beszélni, hogy szerintem nagyon jó tanács egy spielhez, hogy igazából nem arról kell meggyőzni őket, hogy adjanak pénzt, hanem arról, hogy miért érdemes EZT támogatni, és miért szükséges rendszeresen.<br>Nem tudom van-e még olyan ember, akit megvisel, hogyha bunkóznak vele, vagy ignorálják. De hogyha valaki egy olyan emberrel bunkózik, aki másokon akar segíteni, akkor igazából ki is a hülye? Ez csak az ő szegénységi bizonyítványuk, nem a tietek. Meg amúgy pont a napokba gondolkoztam azon, hogyha valaki beszól, én igazából pénzt keresek vele😂 Így is úgy is megkapod az órabért, akkor már nem mindegy? Ahogy írták korábban is, egyszerűen nem érdemlik meg, hogy erre pazaroljátok az energiátokat, mert mindig jönni fog egy kedves ember, akit megéri meggyőzni mert megfog hallgatni☺️❤️<br><br><i> - Enci (Pest)</i>")
+        $("#kep").attr("src","broken.png")
+        break
+    case 7:
+        console.log(7)
+        $('#message').html("Először is azt szeretném mondani, hogy nagyon szuperek vagytok 😊 Ha sok bunkó emberrel találkoztok, jusson eszetekbe vannak kincsemberek, csak meg kell őket találni 🙂<br> Fontos, hogy szívből jöjjön a toborzás, hiszen ezt láthatják rajtatok, nemegyszer kérdezték már, hogy amúgy ezt szívből csinálom-e, mert látszódik, úgyhogy tényleg látszódhat<br> 🙂 Személy szerint nagyon szeretem a gyerekeket és az SOS-t, és hangulata van a toborzásnak 😊 Ha be szoktak jönni a megérzéseid az életben, itt is bejöhetnek, nekem nem mindig jön be, de sokszor és sokszor megéreztem, hogy támogató lesz valaki. <br>Nem vagyok hívő, de hiszek Istenben és volt, hogy nem működött az f2f, de elmondtam magamban egy imát és másodjára sikerült 😊 Nem tudom ezek véletlenek-e vagy tényleg segít, de hittel imádkozok és hittel is kell. ❤<br><br><i> - Anita (Debrecen)</i>")
+        $("#kep").attr("src","broken.png")
+        break
+}
+}
+
+)
+
+$('#elso').click(function() {
+    $('#message').show('slow', function() {
+      // Animation complete.
+    });
+    $('#vonal').animate({'width': '100%'}, 'slow');
+    $('#message').animate('slow');
+  });
+
+  $('#elso').click(function(){
+    var $this = $(this);
+    $this.toggleClass('SeeMore');
+    if($this.hasClass('#elso')){
+        $this.text('See More');         
+    } else {
+        $this.text('Kinyitok még egyet');
+    }
+});
+
